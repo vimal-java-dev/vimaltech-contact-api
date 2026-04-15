7a6e56b1de26e8643a37c6090117464b1a3c9361
vimal-java-dev <vimal929@gmail.com>
2026-04-15 07:59:58 +0530

    Profile(test) and @Profile(prod) added
    Signed-off-by: vimal-java-dev <vimal929@gmail.com>

--- src/main/java/com/vimaltech/contactapi/service/impl/NoOpEmailService.java
+++ src/main/java/com/vimaltech/contactapi/service/impl/NoOpEmailService.java
@@ -3,11 +3,11 @@ package com.vimaltech.contactapi.service.impl;
 import com.vimaltech.contactapi.dto.EmailRequest;
 import com.vimaltech.contactapi.service.EmailService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Primary
+@Profile("test")
 @Slf4j
 public class NoOpEmailService implements EmailService {
 
--- src/main/java/com/vimaltech/contactapi/service/impl/SmtpEmailService.java
+++ src/main/java/com/vimaltech/contactapi/service/impl/SmtpEmailService.java
@@ -5,12 +5,14 @@ import com.vimaltech.contactapi.service.EmailService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("prod")
 @Slf4j
 @ConditionalOnProperty(name = "spring.mail.host")
 public class SmtpEmailService implements EmailService {
